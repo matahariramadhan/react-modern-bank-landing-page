3a33939042f7b164d36bc9b4f9a9458780258e85
matahariramadhan <ramadhan.matahari24@gmail.com>
2022-09-12 21:57:12 +0200

    add get started button in mobile device

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -56,6 +56,11 @@ const Hero = () => {
         <div className="absolute z-1 w-[80%] h-[80%] rounded-full bottom-40 white__gradient"></div>
         <div className="absolute z-0 w-[50%] h-[50%] right-20 bottom-20 blue__gradient"></div>
       </div>
+
+      {/* only visible in ss device */}
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted />
+      </div>
     </section>
   );
 };
