8572eb8869b6bc1c48fadfe0bb2221a8f5c6159a
matahariramadhan <ramadhan.matahari24@gmail.com>
2022-09-12 21:57:30 +0200

    add stats

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
+import { stats } from "../constants";
+import styles from "../style";
+
 const Stats = () => {
-  return <div>Stats</div>;
+  return (
+    <section
+      className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6 mx-3`}
+    >
+      {stats.map((stat) => (
+        <div
+          key={stat.id}
+          className="flex-1 flex flex-row justify-center items-center m-3"
+        >
+          <h4 className="font-poppins font-semibold xs:text-[42px] text-[30px] xs:leading-[53px] leading-[43px] text-white">
+            {stat.value}
+          </h4>
+          <p className="font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] text-gradient uppercase ml-3">
+            {stat.title}
+          </p>
+        </div>
+      ))}
+    </section>
+  );
 };
 
 export default Stats;
