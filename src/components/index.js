adbf827ce3c1046c39e06ed42aed7a04d43c4992
matahariramadhan <ramadhan.matahari24@gmail.com>
2022-09-12 14:46:15 +0200

    add starter components and modify .gitignore

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Billing from "./Billing";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+export {
+  Navbar,
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Footer,
+};
